501ca6194974ddd8c0675d8e2d8660666009485f
David Cervantes <davidcervantesdev@gmail.com>
2024-01-22 17:20:50 -0800

    feat: added headers to response

--- dns.py
+++ dns.py
@@ -16,22 +16,20 @@ def load_zones():
             data = json.load(zonedata)
             zonename = data["$origin"]
             jsonzone[zonename] = data
-    
     return jsonzone
 
 zonedata = load_zones()
 
-def get_flags(flags):
-    
+def getflags(flags):
+
     byte1 = bytes(flags[:1])
     byte2 = bytes(flags[1:2])
 
-    resflags = ''
+    rflags = ''
 
     QR = '1'
 
     OPCODE = ''
-
     for bit in range(1,5):
         OPCODE += str(ord(byte1)&(1<<bit))
 
@@ -49,9 +47,9 @@ def get_flags(flags):
 
     RCODE = '0000'
 
-    return (int(QR + OPCODE + AA + TC + RD, 2).to_bytes(1, byteorder = 'big') + int(RA + Z + RCODE, 2).to_bytes(1, byteorder = 'big'))
+    return int(QR + OPCODE + AA + TC + RD, 2).to_bytes(1, byteorder='big') + int(RA + Z + RCODE, 2).to_bytes(1, byteorder='big')
 
-def get_question_domain(data):
+def getquestiondomain(data):
 
     state = 0
     expectedlength = 0
@@ -59,7 +57,6 @@ def get_question_domain(data):
     domainparts = []
     x = 0
     y = 0
-
     for byte in data:
         if state == 1:
             if byte != 0:
@@ -76,56 +73,100 @@ def get_question_domain(data):
         else:
             state = 1
             expectedlength = byte
-        
         y += 1
 
-    questiontype = data[y: y + 2]
+    questiontype = data[y:y+2]
 
     return (domainparts, questiontype)
 
-def get_zone(domain):
+def getzone(domain):
     global zonedata
 
     zone_name = '.'.join(domain)
     return zonedata[zone_name]
 
-def get_recs(data):
-    domain, questiontype = get_question_domain(data)
+def getrecs(data):
+    domain, questiontype = getquestiondomain(data)
     qt = ''
     if questiontype == b'\x00\x01':
         qt = 'a'
 
-    zone = get_zone(domain)
+    zone = getzone(domain)
 
     return (zone[qt], qt, domain)
 
+def buildquestion(domainname, rectype):
+    qbytes = b''
 
-def build_response(data):
+    for part in domainname:
+        length = len(part)
+        qbytes += bytes([length])
+
+        for char in part:
+            qbytes += ord(char).to_bytes(1, byteorder='big')
+
+    if rectype == 'a':
+        qbytes += (1).to_bytes(2, byteorder='big')
+
+    qbytes += (1).to_bytes(2, byteorder='big')
+
+    return qbytes
+
+def rectobytes(domainname, rectype, recttl, recval):
+
+    rbytes = b'\xc0\x0c'
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([1])
+
+    rbytes = rbytes + bytes([0]) + bytes([1])
+
+    rbytes += int(recttl).to_bytes(4, byteorder='big')
+
+    if rectype == 'a':
+        rbytes = rbytes + bytes([0]) + bytes([4])
+
+        for part in recval.split('.'):
+            rbytes += bytes([int(part)])
+    return rbytes
+
+def buildresponse(data):
 
     # Transaction ID
     TransactionID = data[:2]
-     
-    # Get the Flags
-    Flags = get_flags(data[2:4])
+
+    # Get the flags
+    Flags = getflags(data[2:4])
 
     # Question Count
     QDCOUNT = b'\x00\x01'
 
     # Answer Count
-    ANCOUNT = len(get_recs(data[12:][0])).to_bytes(2, byteorder = 'big')
-    print(ANCOUNT)
+    ANCOUNT = len(getrecs(data[12:])[0]).to_bytes(2, byteorder='big')
 
     # Nameserver Count
-    NSCOUNT = (0).to_bytes(2, byteorder = 'big')
+    NSCOUNT = (0).to_bytes(2, byteorder='big')
 
-    # Additional Count
-    ARCOUNT = (0).to_bytes(2, byteorder = 'big')
+    # Additonal Count
+    ARCOUNT = (0).to_bytes(2, byteorder='big')
 
-    dnsheader = (TransactionID + Flags + QDCOUNT + ANCOUNT + NSCOUNT + ARCOUNT)
+    dnsheader = TransactionID + Flags + QDCOUNT + ANCOUNT + NSCOUNT + ARCOUNT
 
-    print(dnsheader)
+    # Create DNS body
+    dnsbody = b''
+
+    # Get answer for query
+    records, rectype, domainname = getrecs(data[12:])
+
+    dnsquestion = buildquestion(domainname, rectype)
+
+    for record in records:
+        dnsbody += rectobytes(domainname, rectype, record["ttl"], record["value"])
+
+    return dnsheader + dnsquestion + dnsbody
+
 
 while 1:
     data, addr = sock.recvfrom(512)
-    r = build_response(data)
+    r = buildresponse(data)
     sock.sendto(r, addr)
